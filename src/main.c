a698ea8d925d67a75a145f0556876db495b4b9d5
azahar03 <96001060+azahar03@users.noreply.github.com>
2025-12-04 09:46:10 +0100

    Refactor main function for better readability

--- src/main.c
+++ src/main.c
@@ -4,31 +4,30 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-
-	else {printf("Erreur de parametres");}
-	}
+	if (argc == 4){
+		char* op = argv[1];
+		char* a = argv[2];
+		char* b = argv[3];
+		double r = 0;
+		if (strcmp(op, "add") == 0) {
+			r = _add(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "sub") == 0) {
+			r = _sub(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "mul") == 0) {
+			r = _mul(atof(a), atof(b));
+			printf("%lf",r);
+		}
+		else if (strcmp(op, "div") == 0) {
+			r = _div(atof(a), atof(b));
+			printf("%lf",r);
+		}
+	
+		else {printf("Erreur de parametres");}
+		}
 	else if (argc == 3){
 		char* op = argv[1];
 		char* a = argv[2];
