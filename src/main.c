fd73d5df7f5f6a6002fd11ceb16654f139e279a4
azahar03 <96001060+azahar03@users.noreply.github.com>
2025-12-04 09:52:13 +0100

    Add result variable for car operation

--- src/main.c
+++ src/main.c
@@ -31,6 +31,7 @@ int main(int argc, char *argv[]){
 	else if (argc == 3){
 		char* op = argv[1];
 		char* a = argv[2];
+		double r = 0;
 		if (strcmp(op, "car") == 0) {
 			r = _car(atof(a));
 			printf("%lf",r);
